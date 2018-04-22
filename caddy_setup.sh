baa3b773952f751f6b998953f1d4fd5cd0261f8f
SquadraSec <pboerman2897@outlook.com>
2018-04-22 21:07:53 +0200

    Add files via upload

---
+++ caddy_setup.sh
@@ -0,0 +1,115 @@
+apt install curl
+curl https://getcaddy.com | bash -s personal dyndns,hook.service,http.authz,http.awses,http.awslambda,http.cache,http.cgi,http.cors,http.datadog,http.expires,http.filemanager,http.filter,http.forwardproxy,http.geoip,http.git,http.gopkg,http.grpc,http.hugo,http.ipfilter,http.jekyll,http.jwt,http.locale,http.login,http.mailout,http.minify,http.nobots,http.prometheus,http.proxyprotocol,http.ratelimit,http.realip,http.reauth,http.restic,http.upload,http.webdav,net,tls.dns.cloudflare
+chown root:root /usr/local/bin/caddy
+chmod 755 /usr/local/bin/caddy
+setcap 'cap_net_bind_service=+eip' /usr/local/bin/caddy
+mkdir -p /etc/caddy
+chown -R root:www-data /etc/caddy
+mkdir -p /etc/ssl/caddy
+chown -R www-data:root /etc/ssl/caddy
+chmod 770 /etc/ssl/caddy
+touch /etc/caddy/Caddyfile
+mkdir -p /var/www/{dl,cloud}.squadrasec.com/html
+chown www-data:www-data /var/www
+chmod 755 /var/www
+sudo wget https://raw.githubusercontent.com/mholt/caddy/master/dist/init/linux-systemd/caddy.service
+sudo cp caddy.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/caddy.service
+sudo chmod 644 /etc/systemd/system/caddy.service
+sudo systemctl daemon-reload
+sudo touch /etc/caddy/Caddyfile
+
+CFILE="/etc/caddy/Caddyfile"
+
+/bin/cat <<EOM >$CFILE
+
+squadrasec.com {
+    redir https://www.squadrasec.com{uri}
+}
+
+https://www.squadrasec.com {
+
+redir 301 {
+	if {>X-Forwarded-Proto} is http
+	/  https://{host}{uri}
+}
+
+    tls pboerman2897@outlook.com
+    proxy / 127.0.0.1:2368 {
+        websocket
+    }
+}
+
+cloud.squadrasec.com {
+    redir https://www.cloud.squadrasec.com{uri}
+}
+
+https://www.cloud.squadrasec.com {
+
+redir 301 {
+	if {>X-Forwarded-Proto} is http
+	/  https://{host}{uri}
+}
+	
+	gzip
+	tls    pboerman2897@outlook.com
+	root   /var/www/nextcloud
+	log    /var/log/nextcloud_access.log
+	errors /var/log/nextcloud_errors.log
+
+	fastcgi / 127.0.0.1:9000 php {
+		env PATH /bin
+	}
+	
+	# checks for images
+        rewrite {
+	        ext .svg .gif .png .html .ttf .woff .ico .jpg .jpeg
+		r ^/index.php/(.+)$
+		to /{1} /index.php?{1}
+	}
+
+	rewrite {
+		r ^/index.php/.*$
+		to /index.php?{query}
+	}
+
+	# client support (e.g. os x calendar / contacts)
+	redir /.well-known/carddav /remote.php/carddav 301
+	redir /.well-known/caldav /remote.php/caldav 301
+
+	# remove trailing / as it causes errors with php-fpm
+	rewrite {
+		r ^/remote.php/(webdav|caldav|carddav|dav)(\/?)$
+		to /remote.php/{1}
+	}
+
+	rewrite {
+		r ^/remote.php/(webdav|caldav|carddav|dav)/(.+?)(\/?)$
+		to /remote.php/{1}/{2}
+	}
+
+	rewrite {
+		r ^/public.php/(dav|webdav|caldav|carddav)(\/?)$
+		to /public.php/{1}
+	}
+
+	rewrite {
+		r ^/public.php/(dav|webdav|caldav|carddav)/(.+)(\/?)$
+		to /public.php/{1}/{2}
+	}
+
+	# .htaccess / data / config / ... shouldn't be accessible from outside
+	status 403 {
+		/.htacces
+		/data
+		/config
+		/db_structure
+		/.xml
+		/README
+	}
+
+	header / Strict-Transport-Security "max-age=31536000;"
+
+}
+
+EOM
